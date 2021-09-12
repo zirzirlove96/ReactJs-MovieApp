e2f3c6b9e194a2ea8dd335dfc7f31fc24c762c0a
김지영 <zirzirlove96@github.com>
2021-09-12 21:28:09 +0900

    Second Lecture React.Js State Function

--- src/App.js
+++ src/App.js
@@ -1,96 +1,52 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import PropsTypes from 'prop-types';
 
-/**
-function Food(dish){
-  return <Potato name={dish.name} description={dish.desc}/>
+function Food({name, picture, rating}){
+  return (
+  <div>
+    <h2>I Like {name}</h2>
+    <h4>{rating} / 5.0</h4>
+    <img src={picture}/>
+  </div>
+  );
 }
- */
 
- 
-const favFood = [
+Food.PropsTypes = {
+  name : PropsTypes.string.isRequired,
+  picture : PropsTypes.string.isRequired,
+  rating : PropsTypes.number.isRequired
+}
+
+const foodList = [
   {
-    id: 1,
-    name: "Baicon",
-    desc: "This is Baicon",
+    id:1,
+    name: "kimchi",
+    image: "https://www.seriouseats.com/thmb/KoF1E0wKOWhPLGaSy0gI3hZAXfk=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/20210527-baechu-kimchi-vicky-wasik-seriouseats-seriouseats-3-18a2d6d7d1d74a7a82cb13ed350218be.jpg",
     rating: 4.7
   },
   {
-    id: 2,
-    name: "Kimchi",
-    desc: "This is Kimchi",
-    rating: 3.5
-  }];
-
-function Potato({ name, description, rating }) {
-  return <div>
-    <h1>I Love {name}</h1>
-    <h3>{description}</h3>
-    <h4>점수 : {rating}</h4>
-  </div>
-}
-
-Potato.propTypes = {
-  name: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-};
-
-class App extends React.Component {
-  state={
-    count: 0
+    id:2,
+    name: "bibimbab",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Dolsot-bibimbap.jpg/330px-Dolsot-bibimbap.jpg",
+    rating:5.0
   }
+]
+//Array 형식 
 
-  add = () => {
-    /**this.setState({count:this.state.count+1})*/
-    this.setState(current => ({count: current.count + 1}))
-    /**this.state.count를 사용하지 않고 current가 this.state.count가 되어
-     * 직접 사용하지 않게 해준다.
-     */
+/*function renderFood(dish)
+{
+ return <Food name={dish.name} picture={dish.image}/> ;
+}*/
 
-  }
-
-  minus = () => {
-    this.setState(current => ({count:current.count-1}))
-  }
-  /**add, minus function을 이용 , 
-   * setState를 이용하여 render는 refresh 되고 
-   * render함수를 호출해 준다.
-   */
-
-  render() {
-    return <div>
-        <h1>The number is: {this.state.count}</h1>
-          <button onClick={this.add}>Add</button>
-          <button onClick={this.minus}>Minus</button>
-          </div>
-  }
-}
-
-/**
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello This is React Course</h1>
-
-    {react에서는 javascript+html과 비슷하게 진행하는데
-      여기에서 component에 값을 전달하여 component에서 값을 
-      html로 밀어넣어준다.
-      name="Baicon"이라는 property를 주었을 때 javascript의 함수와 같이
-      받아낸다.
-     
-    <Potato name="Kimchi"/>
-    <Potato name="Ramen"/>
-      }
-
-      {favFood.map(food => (
-        <Potato key={food.id} name={food.name} description={food.desc} rating={food.rating} />
+function App(){
+  return(
+    <div>
+      <h1>Hello!!! Jiyoung!</h1>
+      {foodList.map(dish=>(
+        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
       ))}
-
-      {favFood.map(Food) map은 list처럼 하나씩 출력해주고,
-    Food는 return값으로 jsx 역할을 한다. }
     </div>
   );
-} */
+}
 
 export default App;
--- src/App2.js
+++ src/App2.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-class App2 extends React.Component {
-    state= {
+//React Component에서 확장된 상황
+//class Component로 react Componentn로 부터 확장되고 screen에 표시된다. screen에 표시되는 것을 render method 안에 넣어줘야 한다.
+//react는 자동적으로 나의 class component의 render method를 실행한다.
+class App2 extends React.Component
+{
+    state = {
         count:0
-    }
-
+    };
     add = () => {
-        this.setState(current => ({count : current.count+1}))
+        //this.state.count +=1 ;
+        this.setState(current => ({count:current.count+1}));
+        //this.state.count 는 외부의 state에 의존하므로 좋지 않음
+        // current 속성을 사용하여 count를 늘려주기
     }
-
     minus = () => {
-        this.setState(current => ({count : current.count-1}))
+        //this.state.count -= 1;
+        this.setState({count:this.state.count-1});
     }
-
-    render() {
+    render(){
         return <div>
-            <h1>The number is: {this.state.count}</h1>
-            <button onClick={this.add}>ADD</button>
-            <button onClick={this.minus}>MINUS</button>
+            <h1>This Number Is {this.state.count}</h1>
+            <button onClick={this.add}>Add</button>
+            <button onClick={this.minus}>Minus</button>
         </div>
     }
 }
